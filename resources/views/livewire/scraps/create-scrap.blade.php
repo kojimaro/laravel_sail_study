cc2c7979b248fc13efc0d5931b11387124556855
kojimaro <ryokojima7@gmail.com>
2024-10-12 13:05:13 +0900

    💄 scrap作成フォームのセクションタイトルを修正

--- resources/views/livewire/scraps/create-scrap.blade.php
+++ resources/views/livewire/scraps/create-scrap.blade.php
@@ -2,7 +2,7 @@
 
 <div class="w-4/12 rounded shadow-lg bg-white">
     <div class="px-6 py-4 bg-primary text-primary-foreground">
-        <h2 class="font-bold text-xl mb-2">Scraps Create</h2>
+        <h2 class="font-bold text-xl mb-2">Create Scrap</h2>
     </div>
 
     <form wire:submit="save" class="p-2">
