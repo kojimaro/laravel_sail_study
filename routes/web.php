2b41ad0b44dd02dac39a4c643da4e68bfc6ac9c8
kojimaro <ryokojima7@gmail.com>
2024-12-12 14:58:55 +0900

    ✨ s3へのアクセスロジック

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', App\Livewire\Scraps\ScrapList::class);
 Route::get('/presigned-url', App\Livewire\Aws\PresignedUrl::class);
+Route::get('/s3', App\Livewire\Aws\S3IamRoleTest::class);
